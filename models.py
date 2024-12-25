06a3ba3f630d3e1b8286d5929f43dc4d32194eb4
firdousfatima131004 <firdousfatima131004@gmail.com>
2024-12-25 22:32:45 +0530

    adding account section design

---
+++ models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column , Integer , String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+     __tablename__ = 'users'
+     id = Column(Integer , primary_key=True)
+     username = Column(String(150) , nullable=False)
+     email = Column(String(150) , nullable=False, unique=True)
+     role = Column(String(150))
+     password = Column(String(150) , nullable=False)
+     
