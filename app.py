f5f9efbee48439d200f27aa597b8e0a5a5b3463e
firdousfatima131004 <firdousfatima131004@gmail.com>
2025-01-05 16:43:43 +0530

    add the respnosive templates

--- app.py
+++ app.py
@@ -144,8 +144,9 @@ def about_us():
      return render_template('aboutus.html',username=username)
 
 
-
-
+@app.route('/blog/')
+def blog():
+    return render_template('blog.html')
 
 
 if __name__ == '__main__':
